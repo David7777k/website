28094a6ab98839ac032ca22eff949ea5965ed1b7
David7777k <davidodnolko@gmail.com>
2025-09-28 21:21:20 +0000

    auto-commit for f027b35d-a58c-429f-b98b-05f62b681d6b

--- panda-next/.next/server/server-reference-manifest.js
+++ panda-next/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DSFYHU1pXRKhIlfZ7PybDdq8/lMrn78DOQagl4oXQa4=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
